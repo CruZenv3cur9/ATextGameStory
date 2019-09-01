f878a957983efd5ad7985685b9f33b04f5997e9a
Simell <simosimell@gmail.com>
2019-09-01 21:43:32 +0200

    ciao

--- ATextGameStory .py
+++ ATextGameStory .py
@@ -1,46 +1,31 @@
+
 import time, sys, os, pickle
 import simpleaudio
 import pygame
-
+import Utility
 # motore di salvataggio
-def saving_crea():
-    try:
-        saving = open("saving.txt", "w")
-    except:
-        saving = open("saving.txt", "x")
-    saving.close()
 
-def saving():  # TODO load data to file
-    global salvataggio_progressi
-    try:
-        saving = open("saving.txt", "w")
-    except:
-        saving = open("saving.txt", "x")
-    saving.write(str(salvataggio_progressi))
-    saving.close()
-    return saving
-    print("\n\n\nSV-OK")
+
 
 
 #creazione saving
 #creazione_file = open("saving.txt", "w")
 #creazione_file.close()
-saving_crea()
+Utility.saving_crea()
 # LISTE  GLOBALI
 progressi_scena1 = ["1"]
 lettura  = open("saving.txt", "r")
 lettura2 = str(lettura.read())
 salvataggio_progressi = list(lettura2.split(","))
 lettura.close()
-saving()
-
+Utility.saving()
 
     # INIZIO DEL PRIMO EPISODIO   (prima scena)
 
 # INIZIO DEL PRIMO EPISODIO
 def scena1():
     #starting del file audio
-    file = 'Pisciobbello.mp3'
+    file = 'Piscio.mp3'
     pygame.mixer.init()
     pygame.mixer.music.load(file)
     pygame.mixer.music.play()
@@ -82,7 +67,7 @@ def scena1():
     else:
         salvataggio_progressi.append("1")
         # richiamo motore di saving
-        saving()
+        Utility.saving()
         domanda = input ("premi 1 per   continuare con il prossimo episodio oppure premi INVIO per tornare al menù")
 
         if domanda == "1":
--- Utility.py
+++ Utility.py
@@ -8,24 +8,9 @@ def saving_crea():
 
 def saving():  # TODO load data to file
     global salvataggio_progressi
-    try:
-        saving = open("saving.txt", "w")
-    except:
-        saving = open("saving.txt", "x")
+    saving = open ("saving.txt", "w")
     saving.write(str(salvataggio_progressi))
     saving.close()
+    print("")
     return saving
-    print("\n\n\nSV-OK")
 
-
-#creazione saving
-#creazione_file = open("saving.txt", "w")
-#creazione_file.close()
-saving_crea()
-# LISTE  GLOBALI
-progressi_scena1 = ["1"]
-lettura  = open("saving.txt", "r")
-lettura2 = str(lettura.read())
-salvataggio_progressi = list(lettura2.split(","))
-lettura.close()
-saving()
