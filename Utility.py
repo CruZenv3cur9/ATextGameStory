5fd6cd5df5d367c692065cbceaf8204b3b14e499
Simel <simosimell@gmail.com>
2019-09-02 11:22:20 +0200

    Attenzione non funzionante!!!!!!!!!!!!!

--- Utility.py
+++ Utility.py
@@ -1,4 +1,10 @@
+import
+
+
+
+
 # motore di salvataggio
+salvataggio_progressi = []
 def saving_crea():
     try:
         saving = open("saving.txt", "w")
@@ -13,5 +19,30 @@ def saving():
     saving.close()
     print("SV-OK")
     return saving
+def saving_aggingi():
+    global savataggio_progressi
+
+    if "1" in salvataggio_progressi :
+        domanda = input("premi 1 per   continuare con il prossimo episodio oppure premi INVIO per tornare al menù")
+        if domanda == "1":
+            scena2()
+        # richiamo menu
+        else:
+            menù()
+    else:
+        salvataggio_progressi.append("1")
+
+        # richiamo motore di saving
+        saving()
+        domanda = input ("premi 1 per   continuare con il prossimo episodio oppure premi INVIO per tornare al menù")
+
+        if domanda == "1":
+            scena2()
+        else:
+            menù()
+
+    salvataggio_progressi.append("1")
+
+
 
 #SETUPPARE COLLEGAMENTO TRA LISTE DI FILE DIVERSI
