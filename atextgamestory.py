1bb56bbe90f06be2b012fee1a344bf40c5319aef
Simone Giobellina <54772465+Simell@users.noreply.github.com>
2019-09-02 22:37:35 +0200

    Update atextgamestory.py

--- atextgamestory.py
+++ atextgamestory.py
@@ -1,6 +1,7 @@
 import time, sys, os, pickle
 import pygame
 import Utility
+import Capitolo_1 
 # motore di salvataggio
 
 Utility.saving_crea()
@@ -12,50 +13,8 @@ salvataggio_progressi = list(lettura2.split(","))
 lettura.close()
 Utility.saving()
 
-    # INIZIO DEL PRIMO EPISODIO   (prima scena)
-
-# INIZIO DEL PRIMO EPISODIO
-def scena1():
-    #starting del file audio
-    file = 'Piscio.mp3'
-    pygame.mixer.init()
-    pygame.mixer.music.load(file)
-    pygame.mixer.music.play()
-    time.sleep(0.5)
-    #starting della prima frase
-    frase1 = (
-        "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\EPISODIO 1\n\n\ni tuoi occhi si aprono a fatica.......... \ncome se non li aprissi da anni......\nti senti stanco..... assonnato... e  non sai dove ti trovi............ \npercepisci che l'ambiente intorno a te è grande............ "
-        "\nquando qualcosa inizia a fare del rumore...... \n"
-        "lo senti che si avvicina \ncorrendo .......... \n inizi a sentire il suo respiro \n"
-        "percepisci che è vicino  \nmolto vicino..... con uno scatto involontario balzi indietro \n"
-        "urti con il braccio destro un oggetto solido \ndi forma cubica, spigolosa \n"
-        "l'animale indietreggia quasi come se fosse lui più spaventato di te \n"
-        "e proprio in quel momento una voce metallica inizia a parlare alla tua destra \n"
-        "dice :'nome dell'animale, pacifico, età 3 anni, un piccolo quadrupede simile ad un gatto, pelo schiumoso e leggermente pennato,\n"
-        "agile e veloce si preda principalmente di piccoli animali \n"
-        "a quel punto per istinto allunghi la mano in un gesto amichevole verso la creatura \n"
-        "lui struscia quello che sembra il suo muso sul tuo arto \n"
-        "così come per magia la tua mente si dimentica……..\n "
-        "di ogni cosa, di ogni dubbio, paura, insicurezza \n"
-        "così per un momento la tua vita diviene magnifica…………………\n ")
-    # modulo di scrittura
-
-    for char in frase1:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(0.07)
-    # aggiunta del valore 1
-    global salvataggio_progressi
-    Utility.saving_crea()
-    Utility.saving_aggiungi()
-    # richiamo motore di saving
-    Utility.saving()
-    domanda = input ("premi 1 per   continuare con il prossimo episodio oppure premi INVIO per tornare al menù")
-
-    if domanda == "1":
-        scena2()
-    else:
-        menù()
+# Capitolo 1 start
+Capitolo_1.scena1()
 
 
 # INIZIO DEL SECONDO EPISODIO
@@ -143,7 +102,7 @@ def menù():
    # Cancellazione dati
     elif comando == "999":
         comando2 = input("SEI SICURO? SCRIVI \"Si\" SE CONFERMI.\nNON POTRAI MAI PIU' RECUPERARE I TUOI DATI SE PROCEDI.")
-        if comando2 == "Si":
+        if comando2 == "Si" or "si" or "SI":
             os.remove("saving.txt")
             print("\n\nDATI RIMOSSI CON SUCCESSO!")
             Utility.saving_crea()
