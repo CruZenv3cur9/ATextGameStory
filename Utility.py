ad98d8580d605a38371d796dc0afe00ea885d131
Celante <andrea.cantele2004@gmail.com>
2019-09-03 16:30:02 +0200

    Capitolo 2 + Miglioramento Saving (Work in progress)

--- Utility.py
+++ Utility.py
@@ -1,4 +1,4 @@
-import atextgamestory, sys, time
+import atextgamestory, sys, time, Capitolo_2
 
 
 
@@ -45,7 +45,15 @@ def continuer():
     if "1" in salvataggio_progressi:
         Capitolo_2.scena2()
     else:
-        print("prima di poter accedere all'episodio 2 devi aver completato l'episodio 1")
+        print("\nprima di poter accedere all'episodio 2 devi aver completato l'episodio 1")
+        #countdown
+        countdown = 3
+        print("")
+        time.sleep(2)
+        while countdown != 0:
+            print("tornerai al menù principale tra " + str(countdown) + " secondi")
+            countdown -= 1
+            time.sleep(1)
         atextgamestory.menù()
 
 #SETUPPARE COLLEGAMENTO TRA LISTE DI FILE DIVERSI
