9b35962ef28681f13ef3e0b01d07b2802b80d8b1
Simel <simosimell@gmail.com>
2019-09-03 17:19:05 +0200

    NON FUNZIONANTE modifica delle funzioni di capitolo 2

--- Capitolo_2.py
+++ Capitolo_2.py
@@ -3,15 +3,17 @@ import pygame
 import Utility
 
 
-
+controllo = 0
 # INIZIO DEL SECONDO EPISODIO
 def scena2():
-    try:
-        Utility.continuer()
-    finally:
+    if controllo != 0:
+        controllo = 0
         frase1 = "ciaoooooo"
      # writing
     for char in frase1:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.1)
+    elif controllo == 0:
+        return controllo
+        Utility.continuer()
--- Utility.py
+++ Utility.py
@@ -3,8 +3,6 @@ import os.path
 from os import path
 
 
-
-
 # motore di salvataggio
 salvataggio_progressi = []
 def saving_crea():
@@ -13,7 +11,7 @@ def saving_crea():
     else:
         saving = open("saving.txt", "x")
         saving.close()
-# METTERE A POSTO STA ROBA
+
 def saving():
     saving = open("saving.txt", "w")
     saving.write(str(salvataggio_progressi))
@@ -26,7 +24,7 @@ def saving_aggiungi():
     if "1" in salvataggio_progressi :
         domanda = input("premi 1 per   continuare con il prossimo episodio oppure premi INVIO per tornare al menù")
         if domanda == "1":
-            atextgamestory.scena2()
+            Capitolo_2.scena2()
         # richiamo menu
         else:
             atextgamestory.menù()
@@ -38,12 +36,13 @@ def saving_aggiungi():
         domanda = input ("premi 1 per   continuare con il prossimo episodio oppure premi INVIO per tornare al menù")
 
         if domanda == "1":
-            atextgamestory.scena2()
+            Capitolo_2.scena2()
         else:
             atextgamestory.menù()
 def continuer():
     global salvataggio_progressi
     if "1" in salvataggio_progressi:
+        Capitolo_2.controllo += 1
         Capitolo_2.scena2()
     else:
         print("\nprima di poter accedere all'episodio 2 devi aver completato l'episodio 1")
@@ -57,4 +56,3 @@ def continuer():
             time.sleep(1)
         atextgamestory.menù()
 
-#SETUPPARE COLLEGAMENTO TRA LISTE DI FILE DIVERSI
